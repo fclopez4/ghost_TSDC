df882167e7def44f4a4b2f9947334fd6a946f771
AndresClavijoRo <a.clavijo1@uniandes.edu.co>
2023-11-10 10:31:55 -0500

    refactor: :recycle: Se reescriben las pruebas usando el patron give when then

--- EscenariosCypress/cypress/e2e/login.cy.js
+++ EscenariosCypress/cypress/e2e/login.cy.js
@@ -5,51 +5,78 @@ const login = new Login()
 const dashboard = new Dashboard()
 
 describe("scenery #1 login page", () => {
-  beforeEach(() => {
-    login.visit()
+  context('Given I login in ghost page', () => {
+    beforeEach(() => {
+      login.visit()
+    })
+
+    context("When I type user and password correctly", () => {
+      beforeEach(() => {
+        login.setInputUsername(Cypress.env('usuario'))
+        login.setInputPassword(Cypress.env('password'))
+        login.getSubmitButton().click()
+        cy.wait(1000)
+      })
+
+      it("Then I see the title dashboard", () => {
+        dashboard.getTitle().should("contain.text", "Dashboard")
+      })
+    })
+
+    context("When I type user and password incorrectly", () => {
+      beforeEach(() => {
+        login.setInputUsername(Cypress.env('usuario'))
+        login.setInputPassword('incorrect')
+        login.getSubmitButton().click()
+        cy.wait(1000)
+      })
+
+      it("Then I see the title password incorrect", () => {
+        login.getIncorrectSessionTitle().should(async (element) => {
+          const text = await element.text();
+          return text.includes('Your password is incorrect.') || text.includes('Too many attempts');
+        });
+      })
+    })
+
+    context("When I type user incorrectly", () => {
+      beforeEach(() => {
+        login.setInputUsername("test@test.com")
+        login.setInputPassword(Cypress.env('password'))
+        login.getSubmitButton().click()
+        cy.wait(1000)
+      })
+
+      it("Then I see the title user not found", () => {
+        login.getIncorrectSessionTitle().should("contain.text", "There is no user with that email address.")
+      })
+    })
+
+    context("When I type user and password", () => {
+      beforeEach(() => {
+        login.getSubmitButton().click()
+        cy.wait(1000)
+      })
+
+      it("Then I see the alert fill form", () => {
+        login.getIncorrectSessionTitle().should("contain.text", "Please fill out the form to sign in.")
+      })
+    })
+
+    context("when I click several times on the forget password button", () => {
+      beforeEach(() => {
+        login.setInputUsername("aaaa@bbb.com")
+        login.setInputPassword("incorrect")
+        for (let index = 0; index < 7; index++) {
+          login.getForgotButton().click()
+          cy.wait(1000)
+        }
+      })
+
+      it("Then I see the alert fill form", () => {
+        login.getIncorrectSessionTitle().should("contain.text", "Too many attempts try again")
+      })
+    })
   })
 
-  it("should login user success with valid credentials", () => {
-    login.setInputUsername(Cypress.env('usuario'))
-    login.setInputPassword(Cypress.env('password'))
-    login.getSubmitButton().click()
-    cy.wait(1000)
-    dashboard.getTitle().should("contain.text", "Dashboard")
-  })
-
-  it("should have incorrect session title password incorrect",() => {
-    login.setInputUsername(Cypress.env('usuario'))
-    login.setInputPassword('incorrect')
-    login.getSubmitButton().click()
-    cy.wait(1000)
-    login.getIncorrectSessionTitle().should(async (element) => {
-      const text = await element.text();
-      return text.includes('Your password is incorrect.') || text.includes('Too many attempts');
-    });
-  })
-
-  it("should have message error when fill input user with invalid email adress",() => {
-    login.setInputUsername("aaaa@bbb.com")
-    login.setInputPassword("incorrect")
-    login.getSubmitButton().click()
-    cy.wait(1000)
-    login.getIncorrectSessionTitle().should("contain.text", "There is no user with that email address.")
-  })
-
-  it("should have message error when not fill any input",() => {
-    login.getSubmitButton().click()
-    cy.wait(1000)
-    login.getIncorrectSessionTitle().should("contain.text", "Please fill out the form to sign in.")
-  })  
-
-
-  it("should have error with username not found when click in forgot password",() => {
-    login.setInputUsername("aaaa@bbb.com")
-    login.setInputPassword("incorrect")
-    for (let index = 0; index < 7; index++) {
-      login.getForgotButton().click()
-      cy.wait(1000)
-    }
-    login.getIncorrectSessionTitle().should("contain.text", "Too many attempts try again")
-  })
 })
