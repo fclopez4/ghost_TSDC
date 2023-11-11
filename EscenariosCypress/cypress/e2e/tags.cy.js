c22c7237c4c79e3f84763f4b036b292fbb64f793
fclopez4 <fc.lopez@uniandes.edu.co>
2023-11-11 01:06:15 -0500

    feat: se define el escenario 11

--- EscenariosCypress/cypress/e2e/tags.cy.js
+++ EscenariosCypress/cypress/e2e/tags.cy.js
@@ -5,7 +5,7 @@ import Tags from "../pages/tags"
 const login = new Login()
 const tags = new Tags()
 
-describe('scenario #10 create new tag', () => {
+describe('scenario #10 create tag', () => {
     context('Given I go to tags page', () => {
         let cookieValue;
 
@@ -34,7 +34,7 @@ describe('scenario #10 create new tag', () => {
                 cy.wait(1000)
             })
 
-            it('Then I should be show a message about empty name', () => {
+            it('Then I should be see a message about empty name', () => {
                 tags.getTagMessageError().should("contain.text", "You must specify a name for the tag.")
             })
         })
@@ -44,7 +44,7 @@ describe('scenario #10 create new tag', () => {
                 tags.getTagBreadcrumb().click()
                 cy.wait(1000)
             })
-            it('Then I should be show a modal message', () => {
+            it('Then I should be see a modal message', () => {
                 tags.getTagModalHeader().should("contain.text", "Are you sure you want to leave this page?")
             })
         })
@@ -58,7 +58,7 @@ describe('scenario #10 create new tag', () => {
                 tags.getButtonSaveTag().click()
                 cy.wait(1000)
             })
-            it('Then I should be show a tagname like a title', () => {
+            it('Then I should be see the tag name like a title', () => {
                 tags.getTagTitle().should("contain.text", tagName)
             })
         })
@@ -74,7 +74,7 @@ describe('scenario #10 create new tag', () => {
                 cy.wait(1000)
             })
 
-            it('Then I should be return to tag list and not save the tag',()=>{
+            it('Then I should be return to tag list and not see the tag name in tag list',()=>{
                 tags.getTagTitle().should("contain.text", "Tags")
                 tags.getTagListTitle().contains(tagName).should('not.exist');
             })
@@ -82,3 +82,65 @@ describe('scenario #10 create new tag', () => {
     })
 
 });
+
+describe('scenario #11 edit tag', () => {
+    context('Given I go to tags page', () => {
+        let cookieValue;
+
+        before(() => {
+            login.visit()
+            login.setInputUsername(Cypress.env('usuario'))
+            login.setInputPassword(Cypress.env('password'))
+            login.getSubmitButton().click()
+            cy.wait(3000)
+            cy.getCookie('ghost-admin-api-session').then((cookie) => {
+                cookieValue = cookie.value;
+            });
+        })
+
+        beforeEach(() => {
+            cy.setCookie('ghost-admin-api-session', cookieValue)
+            tags.visit()
+            cy.wait(1000)
+            tags.getButtonNewTag().click()
+            cy.wait(1000)
+        })
+
+        context('When I click on save button after change tag name and return to tag list', () => {
+            let tagName = faker.lorem.slug()
+            beforeEach(() => {             
+                createDummyTag(faker.lorem.slug())
+                tags.getTagNameInput().clear().type(tagName)
+                tags.getButtonSaveTag().click()
+                tags.getTagBreadcrumb().click()
+                cy.wait(1000)
+            })
+
+            it('Then I should be see the new tag name on the tag list', () => {
+                tags.getTagListTitle().contains(tagName).should('exist');
+            })
+        })
+
+        context('When I click on leave button after change tag name without save and return to tag list', () => {
+            let tagName = faker.lorem.slug()
+            beforeEach(() => {
+                createDummyTag(faker.lorem.slug())
+                tags.getTagNameInput().clear().type(tagName)
+                tags.getTagBreadcrumb().click()
+                tags.getTagFooterModalLeaveButton().click()
+                cy.wait(1000)
+            })
+            it('Then I should not be see that new tag name on the tag list', () => {
+                tags.getTagListTitle().contains(tagName).should('not.exist');
+            })
+        })
+    })
+
+});
+
+export function createDummyTag(tagName) {
+    tags.getTagNameInput().type(tagName)
+    tags.getTagColorInput().type(faker.color.rgb({ prefix: '', casing: 'upper' }))
+    tags.getTagDescription().type(faker.lorem.sentences())
+    tags.getButtonSaveTag().click()
+}
